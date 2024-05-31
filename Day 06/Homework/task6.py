b9ba501269d6412b92d1ea17a9138de408267832
saba300 <sabajanana@gmail.com>
2024-05-31 22:19:54 +0400

    sixth commit

---
+++ Day 06/Homework/task6.py
@@ -0,0 +1,10 @@
+'''6)დაწერეთ პროგრამა სადაც მომხმარებელს შემოატანინებთ ინფორმაციას თავის თავზე 
+(name,surname,age,address,gmail) და შემდეგ დაუპრინტეთ ეს ინფორმაცია ერთ დიდ წინადადებად'''
+
+name = input("your name: ")
+surname = input("your surname: ")
+age = input("your age: ")
+address = input("your address: ")
+gmail = input("your gmail: ")
+
+print(name + " " + surname + " " + age + " " + address + " " + gmail)
