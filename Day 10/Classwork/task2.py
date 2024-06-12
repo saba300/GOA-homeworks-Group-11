f20b395431815ea35f72fdc7127a4724b6a51380
saba300 <sabajanana@gmail.com>
2024-06-12 20:19:14 +0400

    tenth commit

---
+++ Day 10/Classwork/task2.py
@@ -0,0 +1,7 @@
+'''მომხმარებელს შემოატანინეთ 5რიცხვი და გაიგეთ ამ რიცხვების ნამრავლი(გამოიყენეთ for ციკლი)'''
+
+sum = 1
+num = int(input("Your number: "))
+for i in range(5):
+    sum = sum*num
+print(sum) 
