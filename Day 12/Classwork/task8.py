87098287b8d84f97bd017e5667b476cc90ffd7ff
saba300 <sabajanana@gmail.com>
2024-06-19 20:40:57 +0400

    commit

---
+++ Day 12/Classwork/task8.py
@@ -0,0 +1,10 @@
+'''შექმენით ისეთი while ციკლი რომელიც 0 დან 10 ის ჩათვლ;ით დაბეჭდავს ყველა რიცხვს და if else  
+გამოყენებით გაიგეთ არის თუ არა ლუწი ან კენტი დასერჩეთ how to know if number is even or odd in python'''
+
+i=0
+while i<=10:
+    print(i)
+    if i%2==0:
+        print("even")
+    elif i%2==1:
+        print("odd")
